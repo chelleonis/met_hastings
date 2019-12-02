18e39211ef54c8e6326a983be172139f4c72a0a7
allenjli <allenjli@umich.edu>
2019-12-01 22:33:38 -0500

    it works now

--- R/met_hastings.R
+++ R/met_hastings.R
@@ -85,8 +85,9 @@
 met_hastings <- function(nsims = 1000, start = 1, burn_in = 0, jump = "normal", jparams = 1,
                          distr = "normal", dparams = 1, likelihood = NULL) {
   #step 1, starting theta
+  cols = length(start)
   theta_current <- start
-  draws <- matrix(rep(NA,nsims), ncol = length(start))
+  draws <- matrix(rep(NA,(nsims+1)*cols), ncol = cols)
   #steps 2-4 in update_theta
   #step 5, repeat for preset number of draws
   for (i in 1:nsims) {
